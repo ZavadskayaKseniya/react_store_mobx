9f1ee94dc2033c0a233a6da6e13792b21df7b5ca
ZavadskayaKseniya <90102809+ZavadskayaKseniya@users.noreply.github.com>
2022-07-13 03:37:45 +0300

    button

--- src/components/card-item.jsx
+++ src/components/card-item.jsx
@@ -1,6 +1,5 @@
 import '../styles/card-item.modules.scss';
 
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -8,12 +7,14 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import React from 'react';
 
+import Buttons from "./buttons";
+
 function CardItem({pill}) {
   return (
     <div className="card-item" >
       <Card sx={{ maxWidth: 250 }}>
         <CardMedia
-          sx={{ backgroundSize: "cover", backgroundRepeat:"no-repeat" }}
+          sx={{ backgroundSize: "cover",backgroundPosition:"center", backgroundRepeat:"no-repeat" }}
           component="img"
           alt="pill img"
           height="140"
@@ -28,7 +29,7 @@ function CardItem({pill}) {
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small">ADD</Button>
+          <Buttons onClick={() => null}>В корзину</Buttons>
         </CardActions>
       </Card>
     </div>
