f4a6017c1cbaaedb2127145b37f035210b54b712
ZavadskayaKseniya <zavadolks@gmail.com>
2022-07-22 05:40:39 +0300

    link

--- src/components/system/Header.tsx
+++ src/components/system/Header.tsx
@@ -3,18 +3,23 @@ import '../../styles/header.modules.scss';
 import {AppBar, Toolbar, Typography} from "@mui/material";
 import React from 'react';
 import { MdAddShoppingCart } from "react-icons/md";
+import {Link} from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
       <AppBar>
         <Toolbar sx={{ justifyContent: "space-between", backgroundColor:"rgba(38,38,38,0.95)" }}>
-          <Typography sx={{"&:hover": {
+          <Link to="/" style={{textDecoration:'none',color: "white"}}>
+            <Typography sx={{"&:hover": {
 
               color: "rgba(150,150,150,0.85)",
               cursor: "pointer"
             }}}>Lekař</Typography>
-          <MdAddShoppingCart size={25} className="icon"/>
+          </Link>
+          <Link to="/basket">
+            <MdAddShoppingCart size={25} className="icon"/>
+          </Link>
         </Toolbar>
 
       </AppBar>
